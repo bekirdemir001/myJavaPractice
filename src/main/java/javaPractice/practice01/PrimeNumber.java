3fed32e17d6ed3574ab9c84955a0359a96dd82b6
Bekir Demir <bekirdemir001@gmail.com>
2024-04-23 15:38:06 +0300

    myJavaPractice

---
+++ src/main/java/javaPractice/practice01/PrimeNumber.java
@@ -0,0 +1,48 @@
+package javaPractice.practice01;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    /*
+        Write a Java program to check if a given number is prime or not.
+        A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers.
+        For Example: 5 is a prime. Because 5=1*5 or 5=5*1
+                But: 4 is not a prime. Because 4=1*4, 4=4*1 and 4=2*2
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = scanner.nextInt();
+
+       boolean isPrime = checkPrime(number);
+
+       if (number<2){
+           System.out.println("Please enter a naturel number greater than 1");
+       }else if (isPrime){
+           System.out.println(number + " is a prime number");
+       }else {
+           System.out.println(number + " is not a prime number");
+       }
+
+    }
+
+    public static boolean checkPrime(int n){
+
+        boolean isPrime = true;
+
+        for (int i=2; i<n; i++){
+
+            if (n % i == 0){
+                isPrime = false;
+                break;
+            }
+
+        }
+
+        return isPrime;
+    }
+
+}
