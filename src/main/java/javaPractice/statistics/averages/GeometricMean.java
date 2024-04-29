eac11c74a4573b073d80877d40720ccda1c0f073
Bekir Demir <bekirdemir001@gmail.com>
2024-04-29 11:46:17 +0300

    myJavaPractice

--- src/main/java/javaPractice/statistics/averages/GeometricMean.java
+++ src/main/java/javaPractice/statistics/averages/GeometricMean.java
@@ -1,6 +1,55 @@
 package javaPractice.statistics.averages;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GeometricMean {
 
+    /*
+    Geometric Mean: Write a program that calculate geometric mean.
+    Generally geometric mean of n numbers is the nth root of their product.
+    Geometric mean = (x1 * x2 * x3 * . . . * xn)1/n
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numberList = new ArrayList<>();
+        do {
+            System.out.println("""
+                    ***************Welcome to find Geometric Mean****************
+                    Enter a number!
+                    Press 0 to exit""");
+
+            int number = scanner.nextInt();
+
+            if (number == 0){
+                System.out.println("*************Good Bye********************");
+                break;
+            }else {
+                numberList.add(number);
+            }
+        }while (true);
+
+        getGeometricMean(numberList);
+
+        scanner.close();
+    }
+
+    //Write a method calculate geometric mean
+    public static void getGeometricMean(List<Integer> numberList){
+
+        //Find product of numbers
+        int product = 1;
+        for (Integer w : numberList){
+            product *= w;
+        }
+
+        //Get nth square of product
+        double geometricMean = Math.pow(product, (double) 1 /numberList.size());
+
+        System.out.println("The geometric mean of the number is = " + geometricMean);
+    }
 
 }
--- src/main/java/javaPractice/statistics/averages/HarmonicMean.java
+++ src/main/java/javaPractice/statistics/averages/HarmonicMean.java
@@ -1,7 +1,58 @@
 package javaPractice.statistics.averages;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class HarmonicMean {
 
+    /*
+    Harmonic Mean; Write a program that calculate harmonic mean.
+    The harmonic mean can be expressed as the reciprocal of the arithmetic mean of the reciprocals of the given set of
+    observations.
+    For Example: 1, 4, 4 --> harmonic mean = 2
+     */
 
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numberList = new ArrayList<>();
+
+        do {
+            System.out.println("""
+                    **************Welcome to find Harmonic Mean******************
+                    Enter a number
+                    Press 0 to exit""");
+
+            int number = scanner.nextInt();
+
+            if (number == 0){
+                System.out.println("*******************Good Bye*******************");
+                break;
+            }else {
+                numberList.add(number);
+            }
+
+        }while (true);
+
+        getHarmonicMean(numberList);
+
+        scanner.close();
+    }
+
+    //Create a method calculate harmonic mean
+    public static void getHarmonicMean(List<Integer> numberList){
+
+        double harmonicMean;
+        double denominator = 0.0;
+
+        //Find the denominator in harmonic mean formula
+        for (Integer w : numberList){
+            denominator += (double) 1 / w;
+        }
+
+        harmonicMean = numberList.size() / denominator;
+
+        System.out.println("Harmonic Mean is = " + harmonicMean);
+    }
 }
